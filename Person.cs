3a6828afa032b1eaa5cf9969a4946f4394eb51eb
Andrew-McCall <amccall@qa.com>
2023-01-23 13:48:47 +0000

    Person Class

---
+++ Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFECSharp
+{
+    class Person
+    {
+        public string LocalName { get; private set; }
+        public string Gender { get; private set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age, string gender)
+        {
+            Console.WriteLine("New Person Created");
+            this.LocalName = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public Person(string name, int age)
+        {
+            Console.WriteLine("New Person Created");
+            this.LocalName = name;
+            this.Age = age;
+
+            this.Gender = "N/A";
+        }
+
+        private void Funny()
+        {
+            Console.WriteLine("A fish swam into a concrete wall, He said Dam!");
+        }
+
+        public void Hello(string name)
+        {
+            Console.WriteLine($"Hello, {name}");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine(string.Format("Hello, {0}. I am {1} years old", this.LocalName, this.Age));
+        }
+
+        public static bool Compare(string nameOne, string nameTwo)
+        {
+            return nameOne.Equals(nameTwo);
+        }
+
+    }
+}
--- Runner.cs
+++ Runner.cs
@@ -12,18 +12,42 @@ namespace DFECSharp
         public static void Main(string[] args)
         {
 
-            // Make new class. 
+            Person David = new Person("David", 20);
+            David.Hello();
+            David.Hello("McCall");
+
+            Person Andrew = new Person("Andrew",18, "Male");
+            Andrew.Hello();
+            Console.WriteLine($"{Andrew.Gender} - {David.Gender}");
             
+            //David.LocalName = "Not Andrew";
+            //David.HelloSelf();
+
+            /// Make new class. 
+
+
             // private void
             //   - Console.WriteLine something funny
 
             // public void
             //   - Say's hello to a person saved privatley (string variable)
 
+
+            /// Run it from main. 
+            // ClassName name = new ClassName();
+            // name.property = x;
+            // name.SayHello();
+
+
             /// Extension
             // public static boolean
             //   - Compare Two given names
 
+
+            /// Extension 2
+            // public function. public property (variable of the class). 
+
+            Console.ReadLine();
         }
 
     }
