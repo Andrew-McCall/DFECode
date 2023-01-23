0f0685337bb14676f7d3a788ab3d7f7415cdda0f
Andrew-McCall <amccall@qa.com>
2023-01-23 11:47:40 +0000

    Varialber class

--- Runner.cs
+++ Runner.cs
@@ -11,34 +11,18 @@ namespace DFECSharp
 
         public static void Main(string[] args)
         {
-            int win1 = 42, win2 = 1, win3 = 5, win4 = 49, win5 = 33, win6 = 34;   
-      
-            string lotteryNumbers = string.Format("The winning lottery numbers are {4}, {1}, {2}, {3}, {4}, {5}", win1, win2, win3, win4, win5, win6);      // 11)           
+
+            // Make new class. 
             
-            string two = $"blah {win1}, {win2 + win3}"; // String literal
-            
-            Console.WriteLine(lotteryNumbers);
-            Console.WriteLine(two);
+            // private void
+            //   - Console.WriteLine something funny
 
-            VariablesClass vc = new VariablesClass();
+            // public void
+            //   - Say's hello to a person saved privatley (string variable)
 
-            Console.WriteLine("|Y|");
-
-            vc.DoY();
-            vc.DoY();
-
-            Console.WriteLine("|Z|");
-
-            vc.DoZ();
-            vc.DoZ();
-            vc.DoZ();
-            vc.DoZ();
-
-            Console.ReadLine();
-            // 0,1 0,1
-            // 1,2
-
-            
+            /// Extension
+            // public static boolean
+            //   - Compare Two given names
 
         }
 
--- VariablesClass.cs
+++ VariablesClass.cs
@@ -10,7 +10,15 @@ namespace DFECSharp
     {
 
         private int x = 0;
-        int y = 1;
+        int z = -1;
+
+        protected int w = 10; // private + some special sub stuff
+
+        public int y = 1; // everyone has access
+        public int s { set; get; } = 0;
+
+        public int t { private set; get; } = 0;
+        public int set { set; protected get; } = 0;
 
         public void DoY()
         {
@@ -27,6 +35,10 @@ namespace DFECSharp
             Console.WriteLine(x.ToString());
         }
 
+        public static bool Compare(int a, int b)
+        {
+            return a == b;
+        }
 
     }
 
