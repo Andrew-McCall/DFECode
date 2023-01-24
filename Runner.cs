70feec7cb7ce7c05235c803cbf80dd0d5991a907
Andrew-McCall <amccall@qa.com>
2023-01-24 10:47:15 +0000

    collections

--- Runner.cs
+++ Runner.cs
@@ -12,13 +12,35 @@ namespace DFECSharp
         public static void Main(string[] args)
         {
 
-            IList<int> myArray = new int[5]; // Default is 0
-            IList<int> myList = new List<int>(5);
+            /// A collection of data. A changable amount of differnt data. (Interface)
 
-            Console.WriteLine(myArray);
-            Console.WriteLine(myList);
+            // Lists
+            int[] myArray = new int[0]; // Array (Part of c#)
+            List<int> myList = new List<int>();
 
-            /// 10Min break. 10:20
+            // Dictionarys 
+            // Key-Value Pairs
+            // "Red"  : "#FF0000"
+            // "Blue" : "#0000FF"
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
+            myDict.Add("Red", "#FF00000");
+
+            // Set
+            SortedSet<int> mySortedSet = new SortedSet<int>();
+            HashSet<int> myHashSet = new HashSet<int>();
+
+            // Queue
+            Queue<int> myQueue = new Queue<int>();
+
+            // Stack
+            Stack<int> myStack = new Stack<int>();
+
+
+
+
+
+
+            /// 10 Min break. 10:20 
 
             // Lists and Collections
 
